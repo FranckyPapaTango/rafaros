d6226469afa61e6ea4dffb57653a8e27f69cd412
FranckyPapaTango <rafaralahyf@gmail.com>
2023-12-26 23:48:13 +0100

    Initial version of Rafaros generated by generator-jhipster@7.9.3

---
+++ src/test/java/com/rafaros/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.rafaros;
+
+import com.rafaros.RafarosApp;
+import com.rafaros.config.AsyncSyncConfiguration;
+import com.rafaros.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { RafarosApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
